25aa77224eb05f1f4d66512164db9f5a6047dde2
cambragol <griffon_academy@yahoo.ca>
2026-02-08 23:47:56 +0900

    Update scripts.cc

--- src/scripts.cc
+++ src/scripts.cc
@@ -1653,7 +1653,7 @@ static int scriptsLoadScriptsList()
 
     // Initialize collision tracking
     bool collisionOccurred = false;
-    char collisionDetails[4096][256] = { { 0 } }; // For tracking collisions
+    static char collisionDetails[4096][256] = { { 0 } };
 
     // same pattern used in art.cc for .dat compatibility - very finicky
     char searchPattern[COMPAT_MAX_PATH];
