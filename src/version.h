2e3aea398f4794142da684ece78de2ddedb69f5d
cambragol <griffon_academy@yahoo.ca>
2026-02-24 07:23:08 +0900

    Update version.h

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@ namespace fallout {
 #define VERSION_MAX (32)
 
 #define VERSION_MAJOR (0)
-#define VERSION_MINOR (.9.1) // update this through PRs?
+#define VERSION_MINOR (9) // update this through PRs?
 #define VERSION_RELEASE ('B') // update this with action?
 
 void versionGetVersion(char* dest, size_t size);
